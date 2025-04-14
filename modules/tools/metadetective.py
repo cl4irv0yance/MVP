4d2d77cfa40a613fde4c0a009fa80b969f484e68
cl4irv0yance <mikesmith12137@gmail.com>
2025-04-14 14:20:29 -0500

    update detective

--- modules/tools/metadetective.py
+++ modules/tools/metadetective.py
@@ -10,11 +10,17 @@ from utils.output import print_section
 def run_metadetective(domain, files_dir, base_dir):
     metadetective_script = get_home_dir() + "/MVP/tools/MetaDetective/src/MetaDetective/MetaDetective.py"
     full_url = "https://" + domain
+    print_section("Beginning File Downloads")
     cmd = ["python3", metadetective_script, "--scraping", "--download-dir", files_dir, "--url", full_url, "--depth", "1"]
     subprocess.run(cmd)
 
-    cmd2 = ["python3", metadetective_script, "-d", files_dir, "-e"] #this exports to HTML with the name MetaData_Report
+    print_section("Getting a total file count")
+    cmd2 = ["python3", metadetective_script, "--scraping", "--scan", "--url", full_url, "--depth", "2"]
     subprocess.run(cmd2)
+
+    print_section("Generating an HTML report.")
+    cmd3 = ["python3", metadetective_script, "-d", files_dir, "-e"] #this exports to HTML with the name MetaData_Report
+    subprocess.run(cmd3)
 
     print_section("Moving the HTML report back to your results directory.")
     report_files = glob.glob("MetaDetective_Export-*.html")
@@ -26,4 +32,3 @@ def run_metadetective(domain, files_dir, base_dir):
     else:
         print("[-] No HTML report found to move.")
 
-    #Maybe need cmd3 here to cp the html export over to the files directory OR to clean out everythig but the HTML export
